3409e86beffd7498e1c44ea6e99eac4b5e58293d
kondo takeshi <chaspy@users.noreply.github.com>
2022-08-09 03:23:41 +0900

    remember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -14,21 +14,8 @@ class SessionsController < ApplicationController
     end
   end
 
-  # 記憶トークンcookieに対応するユーザーを返す
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.encrypted[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
